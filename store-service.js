f7c543f96ee2942c795316e69c90d8d1b2517067
Elthan Nyenti <eenyenti@myseneca.ca>
2025-02-16 01:29:45 -0500

    updated initialize

--- store-service.js
+++ store-service.js
@@ -2,18 +2,32 @@ const fs = require('fs').promises;
 let items = [];
 let categories = [];
 
-async function initialize() {
-    try {
-        const itemsData = await fs.readFile('./data/items.json', 'utf8');
-        items = JSON.parse(itemsData);
+// async function initialize() {
+//     try {
+//         const itemsData = await fs.readFile('./data/items.json', 'utf8');
+//         items = JSON.parse(itemsData);
 
-        const categoriesData = await fs.readFile('./data/categories.json', 'utf8');
-        categories = JSON.parse(categoriesData);
+//         const categoriesData = await fs.readFile('./data/categories.json', 'utf8');
+//         categories = JSON.parse(categoriesData);
         
-        return Promise.resolve(); 
-    } catch (err) {
-        return Promise.reject("Error reading or parsing the files");
-    }
+//         return Promise.resolve(); 
+//     } catch (err) {
+//         return Promise.reject("Error reading or parsing the files");
+//     }
+// }
+function initialize() {
+    return fs.readFile(path.join(__dirname, 'data', 'items.json'), 'utf8')
+        .then(data => {
+            items = JSON.parse(data);
+            return fs.readFile(path.join(__dirname, 'data', 'categories.json'), 'utf8');
+        })
+        .then(data => {
+            categories = JSON.parse(data);
+            return 'Initialization successful';
+        })
+        .catch(err => {
+            throw new Error("Unable to read JSON files: " + err.message);
+        });
 }
 
 function getAllItems() {
