b504c4931b5be22cb20f61635a867ad767fdc01c
Elthan Nyenti <eenyenti@myseneca.ca>
2025-02-16 00:45:57 -0500

    added functionality

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express'); // "require" the Express module
-const app = express(); // obtain the "app" object
+const express = require('express');
+const store = require('./store-service');
+const app = express();
 
-const HTTP_PORT = process.env.PORT || 8080; // assign a port
+const HTTP_PORT = process.env.PORT || 8080;
 const path = require('path');
 
 app.use(express.static('public')); 
@@ -14,5 +15,49 @@ app.get('/about', (req, res) => {
 res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 
-// start the server on the port and output a confirmation to the console
-app.listen(HTTP_PORT, () => console.log(`Express http server listening on ${HTTP_PORT}`));
+app.get('/shop', (req, res) => {
+  // res.send('This is shopping');
+  store.getPublishedItems()
+  .then(publishedItems => {
+    res.json(publishedItems);
+  })
+  .catch(err => {
+    res.status(500).json({message: err});
+  });
+});
+
+app.get('/items', (req, res) => {
+  // res.send('This is items');
+  store.getAllItems()
+  .then(items => {
+    res.json(items);
+  })
+  .catch(err => {
+    res.status(500).json({ message: err }); 
+  });
+});
+
+app.get('/categories', (req, res) => {
+  // res.send('This is categories');
+  store.getCategories()
+  .then(categories => {
+    res.json(categories);
+  })
+  .catch(err => {
+    res.status(500).json({message: err});
+  });
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('PAGE NOT FOUND');
+});
+
+
+store.initialize()
+.then(() => { 
+  // start the server on the port and output a confirmation to the console
+  app.listen(HTTP_PORT, () => console.log(`Express http server listening on ${HTTP_PORT}`));
+})
+.catch(()=>{ 
+  console.log('Failed to load resources :(')
+});
---
+++ store-service.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises;
+let items = [];
+let categories = [];
+
+async function initialize() {
+    try {
+        const itemsData = await fs.readFile('./data/items.json', 'utf8');
+        items = JSON.parse(itemsData);
+
+        const categoriesData = await fs.readFile('./data/categories.json', 'utf8');
+        categories = JSON.parse(categoriesData);
+        
+        return Promise.resolve(); 
+    } catch (err) {
+        return Promise.reject("Error reading or parsing the files");
+    }
+}
+
+function getAllItems() {
+    if (items.length) {
+        return Promise.resolve(items);
+    } else {
+        return Promise.reject('No results returned for items');
+    }
+}
+
+function getPublishedItems() {
+    let publishedItems = items.filter((items) => items.published === true);
+    if (publishedItems.length) {
+        return Promise.resolve(publishedItems);
+    } else {
+        return Promise.reject('No results returned for categories');
+    }
+}
+
+function getCategories() {
+    if (categories.length) {
+        return Promise.resolve(categories);
+    } else {
+        return Promise.reject('No results returned for categories');
+    }
+}
+
+module.exports = 
+{
+    initialize,
+    getAllItems,
+    getPublishedItems,
+    getCategories
+}
